64d5f447c8cd0cab92578087815214e2ac812ae0
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2020-02-25 08:54:43 +0100

    update example

--- dev/test_dev/test-params-between-modules.R
+++ dev/test_dev/test-params-between-modules.R
@@ -1,9 +1,19 @@
-
+library(shiny)
 
 ui <- fluidPage(
   tagList(
-    selectInput('chooseObj', 'choose obj to modify', choices=c('None'='None', 'obj 1'='obj1', 'obj 2'='obj 3', 'obj3'='obj3')),
-    uiOutput('chooseItem_ui'),
+    fluidRow(
+      column(width=4,selectInput('chooseObj', 
+                                  'choose obj to modify', 
+                                  choices=c('None'='None', 'obj 1'='obj1', 'obj 2'='obj2', 'obj3'='obj3'),
+                                 width=150)
+             ),
+      column(width=4,uiOutput('chooseItem_ui')
+             ),
+      column(width=4,actionButton('btn', 'Random change value')
+      )
+    ),
+    
     uiOutput('showVar')
   )
 )
@@ -25,11 +35,37 @@ server <- function(input, output, session) {
     req(input$chooseObj)
     if (input$chooseObj != 'obj1'){return(NULL)}
     
-    selectInput('chooseItem', 'Choose item in object A',choices=c('None'='None', 'A'='A1', 'B'='B', 'C'='C')) 
+    selectInput('chooseItem', 
+                'Choose item in object A',
+                choices=c('None'='None', 'A'='A', 'B'='B', 'C'='C'),
+                width = 150) 
   })
   
   
+  observeEvent(input$btn,{
+    input$chooseObj
+    input$chooseItem
+    
+    if (input$chooseObj == 'obj2'){
+      r$obj2 <- sample(20:29, 1)
+    } else if(input$chooseObj == 'obj3'){
+      r$obj3 <- sample(30:39, 1)
+    } else if (input$chooseObj == 'obj1'){
+      if (input$chooseItem == 'A'){
+        r$obj1$A <- paste0('A_',sample(1:10,1))
+      } else if (input$chooseItem == 'B') {
+        r$obj1$B <- paste0('B_',sample(1:10,1))
+      } else if (input$chooseItem == 'C'){
+        r$obj1$C <- paste0('C_',sample(1:10,1))
+      }
+    }
+    
+  })
+  
   output$showVar <- renderUI({
+    r$obj1
+    r$obj2
+    r$obj3
     tagList(
       h3("App"),
       p("contenu de la variable r$obj1 :"),
