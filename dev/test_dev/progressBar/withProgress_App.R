dd9f2b324e6c1684ee2373b7f0b92ceb43d10829
Enora <enora.fremy@orange.fr>
2020-06-19 14:59:23 +0200

    ProgressBar ~ok, manage amount+multiples

--- dev/test_dev/progressBar/withProgress_App.R
+++ dev/test_dev/progressBar/withProgress_App.R
@@ -1,8 +1,5 @@
 library(shiny)
 
-#source(file.path('.', 'withProgress_Calcul.R'), local=TRUE)$value
-
-
 ui <- fluidPage(
   actionButton("btn_start",label = "Let's stream"),
   actionButton("btn_stop",label = "Stop"),
@@ -13,35 +10,10 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   cat(NULL,file="sink-steps.txt")
-  #details <- reactiveFileReader(500, NULL, "./sink-steps.txt", readLines)
-  
+  N=3
   rv <- reactiveValues(textstream = c(""),
                        timer = reactiveTimer(500),
                        started = FALSE)
-  
- 
-  observeEvent(input$btn_start, {
-    
-    rv$started <- TRUE
-    system2("Rscript", "withProgress_Calcul.R", wait = FALSE)
-    
-    output$textstream_output <- renderUI({
-      
-      N=3
-      progress <- shiny::Progress$new()
-      on.exit(progress$close())
-      progress$set(message = "Calculation in progress")
-      
-      for (i in 1:N) {
-        progress$inc(1/N, detail = HTML(rv$textstream) )
-      }
-      
-      HTML(rv$textstream)
-    })
-  })
-  
-  observeEvent(input$btn_stop, { rv$started <- FALSE })
-  
   
   observe({
     rv$timer()
@@ -51,19 +23,50 @@ server <- function(input, output, session) {
   })
   
   
+  observeEvent(input$btn_start, {
+    
+    progress <- Progress$new(session)
+    observeEvent(input$btn_stop, { 
+      progress$close()
+    })
+    
+    rv$started <- TRUE
+    
+    system2("Rscript", "withProgress_Calcul.R", wait = FALSE)
+    
+    output$textstream_output <- renderUI({
+      
+      progress$set(value=0, message='plop')
+      #ajouter truc genre, si pareil sinon update
+      # plus gerer amount progress$inc
+      for (i in 1:N) {
+        progress$set(i, detail = gsub("<br/>"," ",rv$textstream))
+      }
+      
+      HTML(rv$textstream)
+      
+    })
+    
+  })
   
   
-  #### pour faire disparaitre fichier sink-steps.txt quand session terminee
+  observeEvent(input$btn_stop, { 
+    rv$started <- FALSE
+    
+  })
+  
+  
+  
+  
+  
   plop <- observe({
     rv$started <- FALSE
   })
-  
   # Remove sink file when session ended
   session$onSessionEnded(function() {
     plop$suspend()
     unlink("sink-steps.txt")
   })
-  
   
 }
 
--- dev/test_dev/progressBar/withProgress_Calcul.R
+++ dev/test_dev/progressBar/withProgress_Calcul.R
@@ -3,12 +3,18 @@ toto <- function(connexion, logFile) {
   if (connexion == T) {sink(logFile, split=T)}
   Sys.sleep(2)
   cat("step0\n")
-  if (connexion == T) {sink(logFile,append=T, split=T)}
   
-  for (i in 1:2) {
-    Sys.sleep(5)
-    cat(paste0("step",i,"\n"))
-  }
+  if (connexion == T) {sink(logFile,append=T, split=T)}
+  Sys.sleep(5)
+  cat("step1\n")
+  
+  if (connexion == T) {sink(logFile,append=T, split=T)}
+  Sys.sleep(10)
+  cat("step2\n")
+  
+  if (connexion == T) {sink(logFile,append=T, split=T)}
+  Sys.sleep(4)
+  cat("step3\n")
   
   if (connexion == T) {sink()}
   
