d466aed377412dfb779ff48df02b5afacf6b18df
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2020-09-02 16:21:34 +0200

    test script for generic treatment module

--- dev/test_dev/test-params-for-methodsUI-in-pipeline.R
+++ dev/test_dev/test-params-for-methodsUI-in-pipeline.R
@@ -7,60 +7,217 @@
 ##
 
 
-
-source(file.path('../../R', 'config.R'), local=TRUE)$value
-source(file.path('../../R', 'global.R'), local=TRUE)$value
-source(file.path('../../R', 'mod_pipe_prot_norm.R'), local=TRUE)$value
-
-
 library(shinyjs)
 library(DAPAR2)
 library(tibble)
 library(QFeatures)
 library(shiny)
 
-##
+
+###############################################################################
 ##
 ## Module which create a little ui for the widgets of a function
 ## 
-##
-mod_params_norm_ui <- function(id){
+###############################################################################
+
+mod_params_QuantileCentering_ui <- function(id){
   ns <- NS(id)
   tagList(
-    
+    uiOutput(ns('QuantileCentering_ui'))
   )
 }
 
 
-mod_params_norm_ui <- function(input, output, session, paramsIn){
+mod_params_QuantileCentering_server <- function(input, output, session, paramsIn){
   ns <- session$ns
   
-  rv <- reactiveValues(
-    paramsIn = NULL,
-    paramsOut = NULL
+  params <- reactiveValues(
+    fun = 'QuantileCentering',
+    args = list(
+      type = "overall",
+      quantile = xxx,
+      subset.norm = NULL
+    )
   )
   
+  
+  observeEvent(paramsIn(), ignoreNULL = FALSE, {
+    
+    if (is.null(paramsIn()) || length(paramsIn())==0){
+      params$fun = 'QuantileCentering'
+      params$args$type = "overall",
+      quantile = xxx,
+      params$args$subset.norm = NULL
+    } else {
+      params$args$type = paramsIn()$args$type
+      params$args$quantile = paramsIn()$args$quantile
+      params$args$subset.norm = paramsIn()$args$subset.norm
+    }
+  })
+  
+  
+  
+  observeEvent(input$normalization.type, ignoreInit=TRUE,{params$args$type <- input$normalization.type})
+  
+  
+  output$loess_ui <- renderUI({
+    
+    tagList(
+      selectInput(ns("normalization.type"), "Normalization type",  
+                  choices = c("None", "overall", "within conditions"), 
+                  selected = params$args$type)
+    )
+  })
+  
+  return(reactive({params}))
 }
 
 
 
 
+
+
+
+
+
+###############################################################################
 ##
+## Module which create a little ui for the widgets of a function
+## 
+###############################################################################
+
+mod_params_vsn_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    uiOutput(ns('vsn_ui'))
+  )
+}
+
+
+mod_params_vsn_server <- function(input, output, session, paramsIn){
+  ns <- session$ns
+  
+  params <- reactiveValues(
+    fun = 'vsn',
+    args = list(
+      type = "overall"
+    )
+  )
+  
+  
+  observeEvent(paramsIn(), ignoreNULL = FALSE, {
+    
+    if (is.null(paramsIn()) || length(paramsIn())==0){
+      params$fun = 'vsn'
+      params$args$type = "overall"
+    } else {
+      params$args$type = paramsIn()$args$type
+    }
+  })
+  
+  
+  
+   observeEvent(input$normalization.type, ignoreInit=TRUE,{params$args$type <- input$normalization.type})
+  
+  
+  output$loess_ui <- renderUI({
+    
+    tagList(
+      selectInput(ns("normalization.type"), "Normalization type",  
+                  choices = c("None", "overall", "within conditions"), 
+                  selected = params$args$type)
+    )
+  })
+
+  return(reactive({params}))
+}
+
+
+
+
+
+
+###############################################################################
+##
+## Module which create a little ui for the widgets of a function
+## 
+###############################################################################
+
+mod_params_LOESS_ui <- function(id){
+  ns <- NS(id)
+   tagList(
+     uiOutput(ns('loess_ui'))
+   )
+}
+
+
+mod_params_LOESS_server <- function(input, output, session, paramsIn){
+  ns <- session$ns
+  
+  params <- reactiveValues(
+    fun = 'loess',
+    args = list(
+      span = 0.7,
+      type = "overall"
+    )
+  )
+  
+  
+  observeEvent(paramsIn(), ignoreNULL = FALSE, {
+
+    if (is.null(paramsIn()) || length(paramsIn())==0){
+      params$fun = 'loess'
+      params$args$span=0.7
+      params$args$type = "overall"
+    } else {
+      params$args$span= paramsIn()$args$span
+      params$args$type = paramsIn()$args$type
+    }
+  })
+  
+
+  
+  observeEvent(input$spanLOESS, ignoreInit=TRUE,{ params$args$span <- input$spanLOESS })
+  observeEvent(input$normalization.type, ignoreInit=TRUE,{params$args$type <- input$normalization.type})
+    
+    
+  output$loess_ui <- renderUI({
+
+    tagList(
+      selectInput(ns("normalization.type"), "Normalization type",  
+                choices = c("None", "overall", "within conditions"), 
+                selected = params$args$type),
+    textInput(ns("spanLOESS"), "Span",value = params$args$span)
+    )
+  })
+    
+  
+  
+  return(reactive({params}))
+}
+
+
+
+
+###############################################################################
 ##
 ## Light module of normalisation
 ## 
-##
+###############################################################################
 
-mod_pipe_prot_norm_ui <- function(id){
+mod_norm_ui <- function(id){
   ns <- NS(id)
   tagList(
-    uiOutput(ns('Screen_Prot_norm_1'))
+    shinyjs::useShinyjs(),
+    uiOutput(ns('Screen_Prot_norm_1')),
+    actionButton(ns('reset_btn'), 'Reset'),
+    actionButton(ns('set_btn'), 'Set')
   )
 }
 
 
 
-mod_pipe_prot_norm_server <- function(input, output, session, obj, ind){
+mod_norm_server <- function(input, output, session, obj, ind){
   ns <- session$ns
   
   
@@ -68,237 +225,79 @@ mod_pipe_prot_norm_server <- function(input, output, session, obj, ind){
   rv.norm <- reactiveValues(
     name = "processProtNorm",
     dataIn = NULL,
-    i = NULL,
-    dwidgets = list(method = "None",
-                   type = "None",
-                   varReduction = FALSE,
-                   quantile = 0.15,
-                   spanLOESS = 0.7)
+    dataOut = NULL,
+    tmp = NULL,
+    widgets = list(method = "None"),
+    params = list()
   )
   
-  
-  # observeEvent(input$btn_reset,{
-  #   
-  #   rv.norm$widgets$method <- "None"
-  #   rv.norm$widgets$type <- "None"
-  #   rv.norm$widgets$varReduction <- FALSE
-  #   rv.norm$widgets$quantile <- 0.15
-  #   rv.norm$widgets$spanLOESS <- 0.7
-  # 
-  #   rv.norm$dataIn <- obj()
-  #   rv.norm$i <- ind()
-  # })
-  # 
-  
-  
+
   observe({
     ## instanciation of the RV in the module with parameters
     req(obj())
     rv.norm$dataIn <- obj()
-    rv.norm$i <- ind()
+    rv.norm$dataOut <- obj()
   })
   
+  rv.norm$tmp <- callModule(mod_params_LOESS_server, 'params_loess', paramsIn = reactive({rv.norm$params})  )
+  rv.norm$tmp <- callModule(mod_params_LOESS_server, 'params_vsn', paramsIn = reactive({rv.norm$params})  )
+  observeEvent(rv.norm$tmp,{ rv.norm$params <- rv.norm$tmp()})
   
   ###---------------------------------------------------------------------------------###
   ###                                 Screen 1                                        ###
   ###---------------------------------------------------------------------------------###
   output$Screen_Prot_norm_1 <- renderUI({
-    isolate({
-      tagList(
-        div(
-          div(
-            style="display:inline-block; vertical-align: middle; padding-right: 20px;",
-            selectInput(ns("normalization.method"),"Normalization method", 
-                        choices = DAPAR2::normalizeMethods.dapar(), 
-                        selected = rv.norm$widgets$method,
-                        width='200px')
-          ),
-          div(
-            style="display:inline-block; vertical-align: middle; padding-right: 20px;",
-            hidden(selectInput(ns("normalization.type"), "Normalization type",  
-                               choices = c("overall", "within conditions"), 
-                               selected = rv.norm$widgets$type,
-                               width='150px'))
-          ),
-          div(
-            style="display:inline-block; vertical-align: middle; padding-right: 20px;",
-            hidden(textInput(ns("spanLOESS"), "Span",value = rv.norm$widgets$spanLOESS, width='100px')),
-            uiOutput(ns("test_spanLOESS")),
-            uiOutput(ns("choose_normalizationQuantile")),
-            uiOutput(ns("choose_normalizationScaling"))
-          ),
-          div(
-            style="display:inline-block; vertical-align: middle; padding-right: 20px;",
-            hidden(actionButton(ns("perform.normalization"), "Perform normalization", class = actionBtnClass, width="170px"))
-          )
-        )
-        )
-    })
-    
-  })
-  
-  
-  
-  observeEvent(input$normalization.method, ignoreInit=TRUE,{
-    rv.norm$widgets$method <- input$normalization.method
-  })
-  observeEvent(input$normalization.type, ignoreInit=TRUE,{
-    rv.norm$widgets$type <- input$normalization.type
-  })
-  observeEvent(input$normalization.variance.reduction, ignoreInit=TRUE,{
-    rv.norm$widgets$varReduction <- input$normalization.variance.reduction
-  })
-  observeEvent(input$normalization.quantile, ignoreInit=TRUE,{
-    rv.norm$widgets$quantile <- input$normalization.quantile
-  })
-  observeEvent(input$spanLOESS, ignoreInit=TRUE,{
-    rv.norm$widgets$spanLOESS <- input$spanLOESS
-  })
-  
 
-  output$test_spanLOESS <- renderUI({
-    req(rv.norm$widgets$spanLOESS)
-    if (!is.numeric(rv.norm$widgets$spanLOESS)){
-      tags$p("Please choose a number.")
-    }
-  })
-  
-  
-  output$test_normQuant <- renderUI({
-    req(rv.norm$widgets$quantile)
-    if (!is.numeric(rv.norm$widgets$quantile)){
-      tags$p("Please choose a number.")
-    }
+      tagList(
+        selectInput(ns("normalization.method"),"Normalization method", 
+                    choices = c('None'='None', DAPAR2::normalizeMethods.dapar())
+                    ),
+        
+        hidden(div(id=ns("div_params_loess"),mod_params_LOESS_ui(ns("params_loess")))),
+        hidden(div(id=ns("div_params_vsn"),mod_params_LOESS_ui(ns("params_vsn")))),
+        
+        actionButton(ns("perform.normalization"), "Perform normalization"))
   })
   
  
-  
-  output$choose_normalizationQuantile <- renderUI({
-    req(rv.norm$widgets$method)
-    if (rv.norm$widgets$method != "QuantileCentering") { return (NULL)}
-    
-    tagList(
-      textInput(ns("normalization.quantile"), NULL,
-                value = rv.norm$widgets$quantile,width='150px'),
-      uiOutput(ns("test_normQuant"))
-    )
-    
+  observeEvent(input$reset_btn,{
+    rv.norm$params <- NULL
   })
   
-  
-  
-  
-  
-  output$choose_normalizationScaling <- renderUI({
-    req(rv.norm$widgets$method)
-    
-    if (rv.norm$widgets$method == "MeanCentering"){
-      
-      checkboxInput(ns("normalization.variance.reduction"), 
-                    "Include variance reduction",  
-                    value = rv.norm$widgets$varReduction)
-    }
-    
+  observeEvent(input$set_btn,{
+    rv.norm$params <- list(fun='LOESS', args=list(span=10, type='within conditions'))
   })
   
-  
+  observeEvent(input$normalization.method,{
+    rv.norm$widgets$method <- input$normalization.method
+   toggle('div_params_loess', condition=rv.norm$widgets$method=='LOESS')
+   toggle('div_params_vsn', condition=rv.norm$widgets$method=='vsn')
+  })
+ 
   
   ##' Reactive behavior : Normalization of data
   ##' @author Samuel Wieczorek
   observeEvent(input$perform.normalization,{
     rv.norm$widgets$method
     rv.norm$dataIn
-    # isolate({
-    conds <- colData(rv.norm$dataIn)$Condition
     
-    ## the dataset whihc will be normalized is always the original one
-    rv.norm$dataIn <- obj()
-    rv.norm$i <- ind()
     
-    switch(rv.norm$widgets$method, 
-           None = rv.norm$dataIn <- obj(),
-           
-           GlobalQuantileAlignment = {
-             rv.norm$dataIn <- normalizeD(object = rv.norm$dataIn,
-                                          i = rv.norm$i,
-                                          name = "proteins_norm",
-                                          method='GlobalQuantileAlignment'
-             )
-           },
-           
-           QuantileCentering = {
-             quant <-NA
-             if (!is.null(rv.norm$widgets$quantile))
-               quant <- as.numeric(rv.norm$widgets$quantile)
-             
-             rv.norm$dataIn <- normalizeD(object = rv.norm$dataIn, 
-                                          i = rv.norm$i, 
-                                          name = "proteins_norm",
-                                          method = 'QuantileCentering', 
-                                          conds = conds, 
-                                          type = rv.norm$widgets$type,
-                                          quantile = quant
-             )
-             
-           } ,
-           
-           MeanCentering = {
-             rv.norm$dataIn <- normalizeD(object =rv.norm$dataIn,
-                                          i = rv.norm$i, 
-                                          name ="proteins_norm",
-                                          method = 'MeanCentering', 
-                                          conds = conds, 
-                                          type = rv.norm$widgets$type,
-                                          scaling = rv.norm$widgets$varReduction
-             )
-           }, 
-           
-           SumByColumns = {
-             rv.norm$dataIn <- normalizeD(object = rv.norm$dataIn,
-                                          i =rv.norm$i,
-                                          name = "proteins_norm",
-                                          method = 'SumByColumns', 
-                                          conds = conds, 
-                                          type = rv.norm$widgets$type
-             )
-           },
-           
-           LOESS = { 
-             rv.norm$dataIn <- normalizeD(object = rv.norm$dataIn,
-                                          i = rv.norm$i,
-                                          name = "proteins_norm",
-                                          method = 'LOESS', 
-                                          conds = conds, 
-                                          type = rv.norm$widgets$type,
-                                          span = as.numeric(rv.norm$widgets$spanLOESS)
-             )
-           },
-           
-           vsn = {
-             rv.norm$dataIn <- normalizeD(object = rv.norm$dataIn,
-                                          i = rv.norm$i, 
-                                          name = "proteins_norm",
-                                          method = 'vsn', 
-                                          conds = conds, 
-                                          type = rv.norm$widgets$type)
-           }
-    )
-    # })
-    
-    rv.norm$i <- ind() + 1
-    rv.norm$dataOut <- rv.norm$dataIn
+  # browser()
+     ll <- list(object = rv.norm$dataIn,
+                i = length(names(rv.norm$dataIn)),
+                name = "proteins_norm",
+                method = rv.norm$widgets$method,
+                conds = colData(rv.norm$dataIn)$Condition
+                )
+     
+     rv.norm$dataOut <- do.call(normalizeD, append(ll,  rv.norm$params$args) )
+
   })
   
   
   return({reactive(rv.norm$dataOut)})
   
 }
-
-
-
-
-
-
 
 
 
@@ -311,7 +310,11 @@ mod_pipe_prot_norm_server <- function(input, output, session, obj, ind){
 
 ui <- fluidPage(
   tagList(
-    mod_pipe_prot_norm_ui('view')
+    uiOutput('QF'),
+    br(),
+    uiOutput('metadata'),
+    hr(),
+    mod_norm_ui('view')
   )
 )
 
@@ -320,12 +323,25 @@ server <- function(input, output, session) {
   
   utils::data(Exp1_R25_prot, package='DAPARdata2')
   
+  rv <- reactiveValues(
+    res = Exp1_R25_prot
+  )
   
-  callModule(mod_pipe_prot_norm_server,id = "view",
+  rv$res <- callModule(mod_norm_server,id = "view",
              obj= reactive({Exp1_R25_prot}),
              ind = reactive({2})
              )
   
+  
+  output$QF <- renderUI({
+    rv$res
+    print(paste0(names(rv$res()), collapse=', '))
+  })
+  
+  output$metadata <- renderUI({
+    req(metadata(rv$res()[[length(names(rv$res()))]])$Params)
+     print(metadata(rv$res()[[length(names(rv$res()))]])$Params)
+  })
   
 }
 
