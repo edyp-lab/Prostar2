561efbfb6492cbb2b96f37b77b8e7ed8fd6e4b45
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2020-09-18 11:32:24 +0200

    add tests for removing modules

--- dev/test_dev/test_remove_module_3.R
+++ dev/test_dev/test_remove_module_3.R
@@ -42,71 +42,86 @@ remove_module_observers <- function(session, id){
   session$userData[[cnames[ind]]]$destroy()
 } 
 
+
+
+###--------------------------------------------------------------
 mod_test_ui <- function(id) {
   ns <- shiny::NS(id)
   shiny::uiOutput(ns("lmModel"))
 }
 
 
-mod_test_server <- function(id, param, val){
+mod_test_server <- function(id, val){
   moduleServer(
     id,
     function(input, output, session) {
       
       
-      session$userData[[paste0('module_',id,'_observer_1')]] <- observeEvent(val(), {
-        print(paste0('In module ', id(), ', new value = ', val()))
+      session$userData[[paste0(id,'_observer_1')]] <- observeEvent(val(), {
+        print(paste0('In module ', id, ', new value = ', val()))
       })
       
       
-      output[['lmModel']] <- renderUI({
+      output$lmModel <- renderUI({
         ns <- session$ns
-        tags$div(id = environment(ns)[['namespace']],
+        tags$div(id = environment(ns)$namespace,
                  tagList(
                    wellPanel(
-                     tags$div( style="display:inline-block;",
-                               tags$p(paste0('Module ', param(), ' created')),
-                               actionButton(ns('deleteButton'),  '', icon = shiny::icon('times'))
+                     tags$p(paste0('Module ', id, ' created'))
                      )
                    )
-                 )
         )
       })
     }
   )
 }
-  
-  
- #-----------------------------------
+
+
+#-----------------------------------
 
 
 ui <- fluidPage(
   actionButton('addButton', '', icon = icon('plus')),
-  selectInput('test', '', choices=1:15, width='70px')
+  selectInput('test', '', choices=1:15, width='70px'),
+  uiOutput('mod2delete_ui'),
+  actionButton('delButton', 'Delete selected module', icon = icon('times'))
 )
 
 
 server <- function(input, output, session) {
   
-  rv <- reactiveValues(
-    mod = NULL)
+  
+  rv <- reactiveValues(activeMods=NULL)
+  
+  output$mod2delete_ui <- renderUI({
+    rv$activeMods
+    selectInput('mod2delete', 'Module to delete', choices = rv$activeMods, width='100px')
+    
+  })
+  
+ 
   
   observeEvent(input$addButton, {
     id <- paste0('mod_', input$addButton)
-
+    
     insertUI(
       selector = '#addButton',
       where = "beforeBegin",
       ui = mod_test_ui(id)
     )
     
-    mod_test_server(id, param=reactive(id), val = reactive(input$test))
+    mod_test_server(id, val = reactive(input$test))
+    rv$activeMods <- c(rv$activeMods, id)
     
-    observeEvent(input[[paste0(id, '-deleteButton')]], {
-      removeUI(selector = sprintf('#%s', id))
-      remove_shiny_inputs(id, input)
-      remove_module_observers(session, id)
-    })
+    
+  })
+  
+  
+  observeEvent(input$delButton, {
+    removeUI(selector = sprintf('#%s', input$mod2delete))
+    remove_shiny_inputs(input$mod2delete, input)
+    remove_module_observers(session, input$mod2delete)
+    rv$activeMods <- rv$activeMods[-which(rv$activeMods==input$mod2delete)]
   })
 }
 shinyApp(ui = ui, server = server)
