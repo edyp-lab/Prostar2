8527f0569ec60206e47f4156ef98b69b7128c0e1
FREMY Enora <enora.fremy@orange.fr>
2020-09-21 10:21:19 +0200

    creataModule minimum code

---
+++ dev/check_module_process/new_end_server.R
@@ -0,0 +1,10 @@
+
+ return({reactive(rv$dataOut)})
+
+}
+
+## To be copied in the UI
+# mod_pipe_protein_Filtering_ui('pipe_protein_Filtering_ui_1')
+
+## To be copied in the server
+# callModule(mod_pipe_protein_Filtering_server, 'pipe_protein_Filtering_ui_1')
