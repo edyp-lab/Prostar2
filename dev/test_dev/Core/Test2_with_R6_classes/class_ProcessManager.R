f9f5c1ba142bd2a8bb2e36bb4a3a4cb23a89a26b
samWieczorek <samuel.wieczorek@cea.fr>
2020-10-31 04:48:13 +0100

    change some public functions to private

--- dev/test_dev/Core/Test2_with_R6_classes/class_ProcessManager.R
+++ dev/test_dev/Core/Test2_with_R6_classes/class_ProcessManager.R
@@ -5,7 +5,101 @@ ProcessManager <- R6Class(
     global = list(VALIDATED = 1,
                   SKIPPED = -1,
                   UNDONE = 1
-    )
+    ),
+    Wake = function(){ runif(1,0,1)},
+    
+    GetStringStatus = function(status){
+      if (status==private$global$VALIDATED) "Validated"
+      else if (status==private$global$UNDONE) "Undone"
+      else if (status==private$global$SKIPPED) 'Skipped'
+    },
+    
+    Send_Result_to_Caller = function(){
+      self$rv$wake <- private$Wake()
+      
+      self$dataOut$obj <- self$rv$dataIn
+      self$dataOut$name <- self$config$process.name
+      self$dataOut$trigger <- self$rv$wake
+    },
+    
+    Set_Skipped_Status = function(){
+      for (i in 1:length(self$config$mandatory))
+        if (self$config$status[i] != private$global$VALIDATED && private$GetMaxValidated_AllSteps() > i)
+          self$config$status[i] <- private$global$SKIPPED
+    },
+    
+    GetMaxValidated_AllSteps = function(){
+      val <- 0
+      ind <- which(self$config$status == private$global$VALIDATED)
+      if (length(ind) > 0)
+        val <- max(ind)
+      val
+    },
+    
+    GetMaxValidated_BeforeCurrentPos = function(){
+      ind.max <- 0
+      indices.validated <- which(self$config$status == private$global$VALIDATED)
+      if (length(indices.validated) > 0){
+        ind <- which(indices.validated < self$rv$current.pos)
+        if(length(ind) > 0)
+          ind.max <- max(ind)
+      }
+      
+      if (ind.max == 0)
+        ind.max <- 1
+      
+      ind.max
+    },
+    
+    
+    # Test if a process module (identified by its name) has been skipped.
+    # This function is called each time the list config$isDone is updated
+    # because one can know the status 'Skipped' only when a further module
+    # has been validated
+    is.skipped = function(name){
+      pos <- which(name == names(config$mandatory))
+      return(private$GetStatusPosition(pos) == private$global$SKIPPED)
+    },
+    
+    InitializeModule = function(){
+      private$Initialize_Status_Process()
+      self$config$screens <- private$CreateScreens()
+      self$rv$current.pos <- 1
+    },
+    
+    InitializeDataIn = function(){ 
+      self$rv$dataIn <- self$rv$temp.dataIn
+      #self$rv$dataIn <- dataIn()
+    },
+    
+    GetCurrentStepName = reactive({ names(config$mandatory)[self$rv$current.pos] }),
+    
+    Unskip = function(pos){config$status[pos] <- private$global$UNDONE},
+    
+    GetStatusPosition = function(pos){config$status[pos]},
+    # This function cannot be implemented in the timeline module because 
+    # the id of the screens to reset are not known elsewhere.
+    # Trying to reset the global 'div_screens' in the timeline module
+    # does not work
+    ResetScreens = function(){
+      lapply(1:length(self$config$status), function(x){
+        shinyjs::reset(NS(self$id)(names(self$config$mandatory)[x]))
+      })
+    },
+    
+    CreateScreens = function(){
+      setNames(
+        lapply(1:length(self$steps), 
+               function(x){
+                 do.call(uiOutput, list(outputId=NS(self$id)(self$steps[x])))}),
+        self$steps)
+    },
+    
+    
+    Initialize_Status_Process = function(){
+      self$config$status <- setNames(rep(0,length(self$config$status)),
+                                     names(self$config$status))
+    }
   ),
   public = list(id = NULL,
                 config = reactiveValues() ,
@@ -23,11 +117,7 @@ ProcessManager <- R6Class(
                   lapply(names(config), function(x){self$config[[x]] <- config[[x]]})
                 },
                 
-                GetStringStatus = function(status){
-                  if (status==private$global$VALIDATED) "Validated"
-                  else if (status==private$global$UNDONE) "Undone"
-                  else if (status==private$global$SKIPPED) 'Skipped'
-                },
+                
                 
                 # UI
                 ui = function() {
@@ -53,116 +143,21 @@ ProcessManager <- R6Class(
                   )
                 },
                 
-                Wake = function(){ runif(1,0,1)},
+               
                 
-                Initialize_Status_Process = function(){
-                  self$config$status <- setNames(rep(0,length(self$config$status)),
-                                                 names(self$config$status))
-                },
                 
+                # Cannot be a private function because it is used by functions
+                # in the process logics code
                 ValidateCurrentPos = function(){
                   self$config$status[self$rv$current.pos] <- private$global$VALIDATED
-                  self$Set_Skipped_Status()
+                  private$Set_Skipped_Status()
                   #browser()
                   if (self$config$status[length(self$config$status)] == private$global$VALIDATED)
                     # Either the process has been validated, one can prepare data to ben sent to caller
                     # Or the module has been reseted
-                    self$Send_Result_to_Caller()
+                    private$Send_Result_to_Caller()
                 },
                 
-                
-                Set_Skipped_Status = function(){
-                  for (i in 1:length(self$config$mandatory))
-                    if (self$config$status[i] != private$global$VALIDATED && self$GetMaxValidated_AllSteps() > i)
-                      self$config$status[i] <- private$global$SKIPPED
-                },
-                
-                Send_Result_to_Caller = function(){
-                  self$rv$wake <- self$Wake()
-                  
-                  self$dataOut$obj <- self$rv$dataIn
-                  self$dataOut$name <- self$config$process.name
-                  self$dataOut$trigger <- self$rv$wake
-                },
-                
-                
-                GetMaxValidated_AllSteps = function(){
-                  val <- 0
-                  ind <- which(self$config$status == private$global$VALIDATED)
-                  if (length(ind) > 0)
-                    val <- max(ind)
-                  val
-                },
-                
-                GetMaxValidated_BeforeCurrentPos = function(){
-                  ind.max <- 0
-                  indices.validated <- which(self$config$status == private$global$VALIDATED)
-                  if (length(indices.validated) > 0){
-                    ind <- which(indices.validated < self$rv$current.pos)
-                    if(length(ind) > 0)
-                      ind.max <- max(ind)
-                  }
-                  
-                  if (ind.max == 0)
-                    ind.max <- 1
-                  
-                  ind.max
-                },
-                
-                
-                # Test if a process module (identified by its name) has been skipped.
-                # This function is called each time the list config$isDone is updated
-                # because one can know the status 'Skipped' only when a further module
-                # has been validated
-                is.skipped = function(name){
-                  pos <- which(name == names(config$mandatory))
-                  return(GetStatusPosition(pos) == private$global$SKIPPED)
-                },
-                
-                InitializeModule = function(){
-                  self$Initialize_Status_Process()
-                  self$config$screens <- self$CreateScreens()
-                  self$rv$current.pos <- 1
-                },
-                
-
-                
-                GetCurrentStepName = reactive({ names(config$mandatory)[self$rv$current.pos] }),
-                
-                Unskip = function(pos){config$status[pos] <- private$global$UNDONE},
-                
-                GetStatusPosition = function(pos){config$status[pos]},
-                
-                # This function cannot be implemented in the timeline module because 
-                # the id of the screens to reset are not known elsewhere.
-                # Trying to reset the global 'div_screens' in the timeline module
-                # does not work
-                ResetScreens = function(){
-                  lapply(1:length(self$config$status), function(x){
-                    shinyjs::reset(NS(self$id)(names(self$config$mandatory)[x]))
-                  })
-                },
-                
-                CreateScreens = function(){
-                  setNames(
-                    lapply(1:length(self$steps), 
-                           function(x){
-                             do.call(uiOutput, list(outputId=NS(self$id)(self$steps[x])))}),
-                    self$steps)
-                },
-                
-                InitializeDataIn = function(){ 
-                  self$rv$dataIn <- self$rv$temp.dataIn
-                  #self$rv$dataIn <- dataIn()
-                },
-                
-                ##########################################################################"
-                #####################################################################
-                ## screens of the module
-                ##
-                ############### SCREEN 1 ######################################
-                
-                ################################################################################
                 
                 # SERVER
                 server = function(dataIn = NULL, 
@@ -187,9 +182,12 @@ ProcessManager <- R6Class(
                   
                   observeEvent(req(dataIn()), ignoreNULL=T, ignoreInit = F, { 
                     
+                    # This variable serves as a tampon while waiting the user click on the
+                    # validate button in the Description screen.
+                    # Once done, this variable is observed and the real rv$dataIn function can be
+                    # instanciated
                     self$rv$temp.dataIn <- dataIn()
                     
-                    print('event detected on dataIn()')
                     # Test if input is NA or not
                     inputExists <- length(self$config$status) > 0
                     
@@ -201,12 +199,12 @@ ProcessManager <- R6Class(
                     if (tmpExists){
                       # this case is either the module is skipped or validated
                       #self$rv$current.pos <- length(self$config$status)
-                      self$rv$wake <- self$Wake()
+                      self$rv$wake <- private$Wake()
                     } else {
                       if (inputExists){
                         # The current position is pointed on a new module
                         print("launch case 3")
-                        self$InitializeModule()
+                        private$InitializeModule()
                         InitializeTimeline()
                       } else if (!inputExists){
                         # Initialization of Prostar
@@ -239,19 +237,24 @@ ProcessManager <- R6Class(
                     
                     #--- Catch a reset from timeline or caller
                     observeEvent(req(c(self$rv$timeline.res$reset()!=0, remoteReset()!=0)), {
-                      self$ResetScreens()
+                      private$ResetScreens()
                       self$rv$dataIn <- NA
-                      self$Initialize_Status_Process()
-                      self$Send_Result_to_Caller()
-                      self$InitializeDataIn()
+                      private$Initialize_Status_Process()
+                      private$Send_Result_to_Caller()
+                      private$InitializeDataIn()
                     })
                   }
                   
                   
-                  
-                  
-                  
-                  
+                  observeEvent(isSkipped(), ignoreInit = T, {
+                    if (isSkipped())
+                      tag <- private$global$SKIPPED
+                    else
+                      tag <- private$global$UNDONE
+                    
+                    self$config$status <- setNames(rep(tag,length(self$config$status)),
+                                                   names(self$config$status))
+                  })
                   
                   # MODULE SERVER
                   moduleServer(self$id, function(input, output, session) {
@@ -313,23 +316,15 @@ ProcessManager <- R6Class(
                     output$show_status <- renderUI({
                       req(self$config$status, self$rv$current.pos)
                       tagList(lapply(1:length(self$config$status), 
-                                     function(x){if (x == self$rv$current.pos) tags$p(tags$b(paste0('-> ',names(self$config$status)[x], ' - ', self$GetStringStatus(self$config$status[[x]]))))
-                                       else tags$p(paste0(names(self$config$status)[x], ' - ', self$GetStringStatus(self$config$status[[x]])))
+                                     function(x){if (x == self$rv$current.pos) tags$p(tags$b(paste0('-> ',names(self$config$status)[x], ' - ', private$GetStringStatus(self$config$status[[x]]))))
+                                       else tags$p(paste0(names(self$config$status)[x], ' - ', private$GetStringStatus(self$config$status[[x]])))
                                      }))
                     })
                    
                     #################################################
                     # Main listener of the module which initialize it
                     
-                    observeEvent(isSkipped(), ignoreInit = T, {
-                      if (isSkipped())
-                        tag <- private$global$SKIPPED
-                      else
-                        tag <- private$global$UNDONE
-                      
-                      self$config$status <- setNames(rep(tag,length(self$config$status)),
-                                                     names(self$config$status))
-                    })
+                    
                     
                     
                     GetValidationBtnIds <- reactive({validated.btns <- grep('_validate_btn', names(input))})
