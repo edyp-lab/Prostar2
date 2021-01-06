742184cb72787f266d5e9bcecdadc342818d39ca
FREMY Enora <enora.fremy@orange.fr>
2021-01-06 12:06:52 +0100

    tl vertical rotate 45deg

--- dev/prostar_NvCSS/app_v_timeline_open_modal.R
+++ dev/prostar_NvCSS/app_v_timeline_open_modal.R
@@ -1,8 +1,8 @@
 library(shiny)
 library(shinyWidgets)
 library(shinyjs)
-setwd("~/TELETRAVAIL/github_DAPARforFeatures/Prostar2/dev/prostar_NvCSS/")
-#setwd("~/Github/AdaptedForFeatures/Prostar2/dev/prostar_NvCSS/")
+#setwd("~/TELETRAVAIL/github_DAPARforFeatures/Prostar2/dev/prostar_NvCSS/")
+setwd("~/Github/AdaptedForFeatures/Prostar2/dev/prostar_NvCSS/")
 
 
 ui <- fluidPage(
@@ -16,19 +16,23 @@ ui <- fluidPage(
   
   
   tags$div(class="box",
-           img(src='fleche.PNG', height='20px', width='20px'),
+           #span(HTML("&#10558")),
+           #img(src='fleche.PNG', height='20px', width='20px'),
            tags$div(class="sub_box",
                     p('Filtration')
            ),
-           img(src='fleche.PNG', height='20px', width='20px'),
+           #span(HTML("&#10558")),
+           #img(src='fleche.PNG', height='20px', width='20px'),
            tags$div(class="sub_box",
                     p('Normalization')
            ),
-           img(src='fleche.PNG', height='20px', width='20px'),
+           #span(HTML("&#10558")),
+           #img(src='fleche.PNG', height='20px', width='20px'),
            tags$div(class="sub_box",
                     p('Imputation')
            ),
-           img(src='fleche.PNG', height='20px', width='20px'),
+           #span(HTML("&#10558")),
+           #img(src='fleche.PNG', height='20px', width='20px'),
            tags$div(class="sub_box",
                     p('Aggregation')
            )
@@ -65,31 +69,31 @@ shinyApp(ui, server)
 #   
 #   Voici quelques motivations :
 
-#   2.Plus sérieusement, elle a à mon avis le meilleur facteur de forme
-# (ratio largeur/ hauteur) pour un slide show vertical ; C'est encore plus vrai si on veut à
-# terme que le slideshow puisse être masqué par la barre de menu latérale
+#   2.Plus sÃ©rieusement, elle a Ã  mon avis le meilleur facteur de forme
+# (ratio largeur/ hauteur) pour un slide show vertical ; C'est encore plus vrai si on veut Ã 
+# terme que le slideshow puisse Ãªtre masquÃ© par la barre de menu latÃ©rale
 # 
-#   3.Enfin, elle peut être naturellement adaptée pour faire un slideshow horizontal,
-# et donc avoir des layouts cohérents entre le slideshow du pipeline et du process en cours
+#   3.Enfin, elle peut Ãªtre naturellement adaptÃ©e pour faire un slideshow horizontal,
+# et donc avoir des layouts cohÃ©rents entre le slideshow du pipeline et du process en cours
 # 
 # 
 # 
-# le 5 (à gauche, E: items penchés à 45°)
+# le 5 (Ã  gauche, E: items penchÃ©s Ã  45Â°)
 #   
-#  les motivations ci-dessus s'appliquent aussi (facteur de forme et mise à l'horizontal).
-# Si jamais vous n'aimez pas la diapo 4, voici quelques idées pour améliorer la diapo 5 :
+#  les motivations ci-dessus s'appliquent aussi (facteur de forme et mise Ã  l'horizontal).
+# Si jamais vous n'aimez pas la diapo 4, voici quelques idÃ©es pour amÃ©liorer la diapo 5 :
 #   
 #   -          Mettre des rectangles avec des coins arrondis
 # 
-# -          Remplacer les ronds (qui ne servent à rien parce qu'il s'agit en fait des
-# transitions entre étapes) par des flèches qui suivent un quart de rond, genre comme ça :*
+# -          Remplacer les ronds (qui ne servent Ã  rien parce qu'il s'agit en fait des
+# transitions entre Ã©tapes) par des flÃ¨ches qui suivent un quart de rond, genre comme Ã§a :*
 #   
-#   -          Eventuellement, il est possible d'utiliser la flèche pour indiquer si l'on
-# a le droit de passer à l'étape suivante (en changeant sa couleur ou sa forme, ou en
+#   -          Eventuellement, il est possible d'utiliser la flÃ¨che pour indiquer si l'on
+# a le droit de passer Ã  l'Ã©tape suivante (en changeant sa couleur ou sa forme, ou en
 # rajoutant une croix dessus), et de n'utiliser que 2 couleurs pour les rectangle
-# (fait ou non fait ; en décrivant l'aspect obligatoire ou pas par l'état de la flèche,
-# plutôt que par la couleur du rond de l'étape). D'ailleurs, maintenant que j'y pense,
+# (fait ou non fait ; en dÃ©crivant l'aspect obligatoire ou pas par l'Ã©tat de la flÃ¨che,
+# plutÃ´t que par la couleur du rond de l'Ã©tape). D'ailleurs, maintenant que j'y pense,
 # il y a peut-etre moyen de faire pareil sur la diapo 4, et de changer les arcs de cercles
-# qui composent le serpentin : il serait vert si l'étape suivante est autorisée, rouge sinon,
-# et gris si l'étape a déjà été passé ?
+# qui composent le serpentin : il serait vert si l'Ã©tape suivante est autorisÃ©e, rouge sinon,
+# et gris si l'Ã©tape a dÃ©jÃ  Ã©tÃ© passÃ© ?
 #   
