7e429cfcc588b9c9c2955fc3faae7716d2a6a41b
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-08-21 11:09:53 +0200

    Update convert function

--- inst/workflow/PipelineProtein/examples/example_pipelineProtein_Normalization.R
+++ inst/workflow/PipelineProtein/examples/example_pipelineProtein_Normalization.R
@@ -11,5 +11,12 @@ library(Prostar2)
 data(Exp1_R25_prot, package = 'DaparToolshedData')
 wf.name <- 'PipelineProtein_Normalization'
 wf.path <- system.file('workflow/PipelineProtein', package = 'Prostar2')
+
+
+# Launch in a standalone mode
+shiny::runApp(workflowApp(wf.name, wf.path, dataIn = obj))
+
+
+# Launch in the Magellan workspace
 MagellanNTK(Exp1_R25_prot, wf.path, wf.name)
 
