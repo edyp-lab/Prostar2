72252eadd2e6aa0b66ad8db10e086a70dc753c69
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2025-09-10 16:28:59 +0200

    build site with pkgdown

--- inst/workflow/PipelineProtein/R/PipelineProtein_Description.R
+++ inst/workflow/PipelineProtein/R/PipelineProtein_Description.R
@@ -77,7 +77,7 @@ PipelineProtein_Description_server <- function(id,
         system.file('workflow', package = 'Prostar2'),
         unlist(strsplit(id, '_'))[1], 
         'md', 
-        paste0(id, '.md')))
+        paste0(id, '.Rmd')))
      
       MagellanNTK::process_layout(
         ns = NS(id),
